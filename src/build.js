fa69d449f6e31f20e2cfbc94a274895a32ea7446
jihuayu <8042833@qq.com>
2020-07-15 23:13:23 +0800

    fix build

--- src/build.js
+++ src/build.js
@@ -8,51 +8,51 @@ const fetch = require('node-fetch');
 const adm_zip = require('adm-zip');
 const logger = require('./logger');
 const rimraf = require('rimraf')
+const md5File = require('md5-file')
+
 exports.default = async (includes) => {
     if (!includes) includes = 'default'
     includes += '.json'
     const root = process.cwd();
-    const modpack_temp = path.join(root,".modpack","temp");
-    await rimraf(modpack_temp,async ()=>{
-        const overrides = path.join(root,".modpack","temp","overrides");
+    const modpack_temp = path.join(root, ".modpack", "temp");
+    await rimraf(modpack_temp, async () => {
+        const overrides = path.join(root, ".modpack", "temp", "overrides");
         logger.info(`pack a modpack in ${root}`);
         let mods_cfg;
-        let cfg ;
-        let includes_cfg ;
+        let cfg;
+        let includes_cfg;
         try {
-            mods_cfg = JSON.parse(fs.readFileSync(path.join(root,"modpack-mods.json")));
+            mods_cfg = JSON.parse(fs.readFileSync(path.join(root, "modpack-mods.json")));
             includes_cfg = JSON.parse(fs.readFileSync(path.join(root, "modpack_includes", includes)));
-            cfg = JSON.parse(fs.readFileSync(path.join(root,"modpack-project.json")));
+            cfg = JSON.parse(fs.readFileSync(path.join(root, "modpack-project.json")));
             await makeDir(modpack_temp);
-        }
-        catch (e) {
+        } catch (e) {
             logger.failure(e);
             process.exit();
         }
         let version;
-        if (cfg.modloader === "forge"){
+        if (cfg.modloader === "forge") {
             let versions = (await (await fetch(`https://bmclapi2.bangbang93.com/forge/minecraft/${cfg.mcversion}`)).json())
-            versions = versions.sort((i,j)=>(new Date(i.modified).getTime() > new Date(j.modified).getTime()?-1:1))
+            versions = versions.sort((i, j) => (new Date(i.modified).getTime() > new Date(j.modified).getTime() ? -1 : 1))
             let ans = await inquirer.prompt([
                     {
                         type: 'list',
                         name: 'modloder_version',
                         message: chalk.red('Which modloader version do you like?'),
                         default: 0,
-                        choices:versions.map(i=>`${i.version} at ${i.modified}`)
+                        choices: versions.map(i => `${i.version} at ${i.modified}`)
                     }
                 ]
             )
             version = ans.modloder_version.split(' at ')[0];
-        }
-        else {
+        } else {
             logger.failure("Unsupported modloader found!")
             version = "0.0.0";
         }
         let manifest = {
             minecraft: {
                 version: cfg.mcversion,
-                modLoaders:[{
+                modLoaders: [{
                     id: `${cfg.modloader}-${version}`,
                     primary: true
                 }]
@@ -65,11 +65,27 @@ exports.default = async (includes) => {
             overrides: "overrides"
         }
         if (!includes_cfg.files.includes('mods')) {
-            manifest.files = mods_cfg.map(i=>i.downloadUrl?undefined:{projectID:i.addon_id,fileID:i.file_id,required:true}).filter(i=>i)
+            includes_cfg.files.push('mods')
+            includes_cfg.files = includes_cfg.files.filter(i => fs.existsSync(i));
+            manifest.files = mods_cfg.map(i => i.downloadUrl ? undefined : {
+                projectID: i.addon_id,
+                fileID: i.file_id,
+                required: true,
+                md5: i.md5
+            }).filter(i => i)
             logger.info(`Add ${manifest.files.length} mods to manifest!`)
+            if (includes_cfg.files.length > 0) {
+                await cpy(includes_cfg.files, overrides, {
+                    parents: true, filter: file => {
+                        const md5 = md5File.sync(file.path);
+                        return manifest.files.filter(i => md5 === i.md5).length <= 0;
+                    }
+                });
+            }
+        } else {
+            await cpy(includes_cfg.files, overrides, {parents: true});
         }
-        fs.writeFileSync(`${modpack_temp}/manifest.json`,JSON.stringify(manifest, '\n', 2));
-        await cpy(includes_cfg.files, overrides, { parents:true });
+        fs.writeFileSync(`${modpack_temp}/manifest.json`, JSON.stringify(manifest, '\n', 2));
         let zip = new adm_zip();
 
         zip.addLocalFolder(modpack_temp);
